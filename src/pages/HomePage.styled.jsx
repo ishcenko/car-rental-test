39b0a31ca2ff905dc8f531bfb0eed8bf09f60682
ishcenko <a_ishcenko@i.ua>
2023-11-02 13:39:41 +0200

    Update HomePage.styled.jsx

--- src/pages/HomePage.styled.jsx
+++ src/pages/HomePage.styled.jsx
@@ -71,6 +71,6 @@ export const Button = styled(Link)`
     0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
   &:hover {
     background-color: #021a53;
-    scale: 1.2;
+    // scale: 1.2;
   }
 `;
